e4897483e63c08de2b669ba017d437b98c40b44b
etel <etelethiopia@gmail.com>
2023-10-10 13:30:44 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,19 @@
-#include "main.h"
+#include <main.h>
 
-/**
- * main - function
- * Return: 0
- */
-
-int main(void)
+void print_alphabet(void) 
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+char letter = 'a';
 
+while (letter <= 'z')
+{
+        _putchar(letter);
+        letter++;
+}
+
+    _putchar('\n');
+}
+
+int main() {
+    print_alphabet();
+return (0);
 }
